3d45ee9241a8189d7bff13d46b97161bb2567823
Yvon Cognard <yvon@cognard.com>
2024-12-26 22:52:12 +0100

    refactor: Simplify headers object and add commented SignalR message example

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const signalR = require('@microsoft/signalr');
 require('dotenv').config();
 
+// {"arguments":[{"$type":"updateContext","sessionId":"501d33a2-3ffa-0a18-4170-2f55a96a7f5b","contextKey":"Inspector","contexts":[{"text":"Test"}]}],"target":"SendMessage","type":1}
+//
+
 async function main() {
     const baseUrl = process.env.WS_BASE_URL;
     const credentials = Buffer.from(`${process.env.WS_USERNAME}:${process.env.WS_PASSWORD}`).toString('base64');
-    const headers = {
-        'Authorization': 'Basic ' + credentials
-    };
+    const headers = { 'Authorization': 'Basic ' + credentials };
 
     const connection = new signalR.HubConnectionBuilder()
         .withUrl(`${baseUrl}/hub`, { headers })
